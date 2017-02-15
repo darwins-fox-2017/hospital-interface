93475e38392cf9229c272d9ab7a46fa5d6007dc2
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-02-16 05:42:46 +0700

    Revision 2

--- hospital.js
+++ hospital.js
@@ -19,7 +19,8 @@ class Patient {
 }
 
 class Employee {
-  constructor(name, position, username, password) {
+  constructor(id, name, position, username, password) {
+    this.id       = id
     this.name     = name
     this.position = position
     this.username = username
@@ -65,8 +66,10 @@ class HospitalApp {
     this.hospital          = new Hospital(name, location, employees, patients)
     this._loginStatus
     this._loggedInEmployee
-    this._loginPrivillege
+    this._loginPrivilege
   }
+
+  get loginPrivilege() { return this._loginPrivilege }
   
   fileHandler(option = 'read', fileName) {
     if (!fs.existsSync(fileName)) {
@@ -92,77 +95,181 @@ class HospitalApp {
     return `Welcome to ${this.hospital.name}`+'\n_____________________________\n'
   }
   
-  createEmployee(name,position,username,password) {
-    let oldEmployee     = this.fileHandler('read', 'employees.json')
-    let prepareData     = this.fileHandler('write', 'employees.json') 
-    oldEmployee.push(new Employee(name,position,username,password))
-    prepareData(JSON.stringify(oldEmployee))
-    
+  addNewRecord(file, record) {
+    let oldRecord     = this.fileHandler('read', file)
+    let prepareRecord = this.fileHandler('write', file) 
+    oldRecord.push(record)
+    prepareRecord(JSON.stringify(oldRecord))
   }
-  
-  getEmployees() {
-    return this.fileHandler('read', 'employees.json')
+
+  getData(file) {
+    return this.fileHandler('read', file)
   }
-  
-  getEmployee(index) {
-    return this.getEmployee()[index]
+
+  getByKey(file, key, value) {
+    let records = this.getData(file)
+    for (let i = 0; i < records.length; i++) {
+      if (records[i][key] == value) {
+        return records[i] 
+      }
+    }
+  }
+
+  editRecord(file, key, keyword, targetKey, newVal) {
+    let records = this.getData(file)
+    let name 
+    let newDiagnosis
+    for (let i = 0; i < records.length; i++) {
+      if (records[i][key] == keyword) {
+        records[i][targetKey] = newVal
+        name         = records[i].name
+        newDiagnosis = records[i][targetKey]
+        let prepareRecord = this.fileHandler('write', file) 
+        prepareRecord(JSON.stringify(records))
+      }
+    }
+    return `The ${name}'s diagnosis has been updated`
+  }
+
+  deleteRecord(file, key, keyword) {
+    let records = this.getData(file)
+    let name 
+    for (let i = 0; i < records.length; i++) {
+      if (records[i][key] == keyword) {
+        name = records[i].name
+        records.splice(i, 1)
+        let prepareRecord = this.fileHandler('write', file) 
+        prepareRecord(JSON.stringify(records))
+      }
+    }
+    return `${name} has been deleted from patient records`
   }
   
   login(username, password) {
-    let employees = this.getEmployees()
+    let employees = this.getData('employees.json')
     return password => {
       let login = new Login(username, password)
       if(login.checkAccount(employees)) {
         this._loginStatus      = true
-        this._loginPrivillege  = login.privileged.privilege
+        this._loginPrivilege   = login.privileged.privilege
         this._loggedInEmployee = login.privileged.name
         // console.log(this._loginStatus)
         return this._loginStatus
       }
       return false
     }
-    // let username = readline.question('Please enter your username: ')
-    // let password = readline.question('Please enter your password: ')
-      
+  }
 
-    // for (let i in this.getEmployees()) {
-    //   let uname = this.getEmployee(i).username
-    //   let pass  = this.getEmployee(i).password
-    //   if (username === uname && password === pass) {
-    //     if (this.getEmployee(i).position !== 'office-boy') {
-    //       this.loggedInEmployee = this.getEmployee(i)
-    //       this.appMenu()
-    //     }
-    //     console.log('We are sorry, but you have no access..')
-    //   }
-    // }
+  getCommand(input, privilege, rl, callback) {
+    let defineInput  = input.split(' ')
+    let command      = defineInput[0]
+    let commandParam = defineInput[1]
+
+    if (privilege == 'admin') {
+      switch (command) {
+        case 'list_employees':
+          callback(this.getData('employees.json'), 'Tell me what next to do: ')
+          break;
+        case 'add_employee':   
+          let name        
+          let position
+          let username
+          let password
+          rl.question('name: ', nameInput=> {
+            name = nameInput
+            rl.question('position: ', positionInput=> {
+              position = positionInput
+              rl.question('username: ', usernameInput=> {
+                username = usernameInput
+                rl.question('password: ', passwordInput=> {
+                  password = passwordInput
+                  let employee = this.getData('employees.json')
+                  let lastId = employee.length === 0  ? 0 : employee[employee.length-1].id
+                  this.addNewRecord('employees.json', new Employee(lastId+1, name, position, password))
+                  callback(`${name} has been added`, 'Tell me what next to do: ')
+                })
+              })
+            })
+          })
+          break;   
+        case 'delete_employee':
+            callback(this.deleteRecord('employees.json', 'id', commandParam), 'Tell me what next to do: ')
+          break; 
+        default:
+          
+      }
+    } else {
+      switch (command) {
+        case 'list_patients':
+          callback(this.getData('patients.json'), 'Tell me what next to do: ')
+          break;
+        case 'add_patient':
+          let name        
+          let diagnosis
+          rl.question('name: ', nameInput => {
+            name = nameInput
+            rl.question('diagnosis: ', diagnosisInput => {
+              diagnosis = diagnosisInput
+              let patiens = this.getData('patients.json')
+              let lastId  = patiens.length === 0  ? 0 : patiens[patiens.length-1].id
+              this.addNewRecord('patients.json', new Patient(lastId+1, name, diagnosis))
+              callback(`${name} has been added`, 'Tell me what next to do: ')
+            })
+          })
+          break;
+        case 'view_record':
+          callback(this.getByKey('patients.json', 'id', commandParam), 'Tell me what next to do: ')
+          break;
+        case 'add_record':
+          rl.question('Diagnosis update: ', newDiagnosis =>{
+            callback(this.editRecord('patients.json', 'id', commandParam, 'diagnosis', newDiagnosis), 'Tell me what next to do: ')
+          })
+          break;
+        case 'remove_record':
+            callback(this.deleteRecord('patients.json', 'id', commandParam), 'Tell me what next to do: ')
+          break;
+        default:
+          
+      }
+      
+    }
+
   }
   
-  appMenu() {
+  appMenu(privilege) {
     console.log('_____________________________________________')
-    console.log(`Welcome ${this._loggedInEmployee}, your access level is: ${this._loginPrivillege}`)
+    console.log(`Welcome ${this._loggedInEmployee}, your access level is: ${this._loginPrivilege}`)
     console.log('What would you like to do?')
     console.log('Options:')
-    console.log('list_patients')
-    console.log('view_record <patient_id>')
-    console.log('add_record <patient_id> <complaining>')
-    console.log('remove_record <patient_id>')
+    switch (privilege) {
+      case 'admin':
+        console.log('--list_employees')
+        console.log('--add_employee')
+        console.log('--delete_employee <patient_id>')
+        break;
+      case 'office boy':
+        console.log('You dont have any permission to see medical records') 
+        break;
+      default:
+        console.log('--list_patients')
+        console.log('--add_patient')
+        console.log('--view_record <patient_id>')
+        console.log('--add_record <patient_id>')
+        console.log('--remove_record <patient_id>')
+    }
   }
 }
 
 let hospitalapp = new HospitalApp('Lunatic', 'Subconsciousness', 200, 150)
 
-// hospitalapp.login()
-// hospitalapp.createEmployee('Danang', 'doctor', 'danang', 'admindanang')
-
-let rl = readline.createInterface({
+var rl = readline.createInterface({
   input : process.stdin,
-  output: process.stdout
+  output: process.stdout,
 })
 
 let login
 let loginData = new Object()
-console.log('Welcome')
+hospitalapp.openingMessage()
 rl.setPrompt('Please enter your username: ')
 rl.prompt()
 
@@ -175,13 +282,20 @@ rl.on('line', (input) => {
   } else if (!loginData.hasOwnProperty('password')) {
     loginData['password'] = input
     if (login(loginData.password)) {
-      rl.setPrompt(hospitalapp.appMenu())
+      hospitalapp.appMenu(hospitalapp.loginPrivilege)
+      rl.setPrompt('Tell me what to do: ')
       rl.prompt()
     } else {
       loginData = new Object()
-      console.log('Lu salah account cuk')
+      console.log('Your account is invalid')
       rl.setPrompt('Please enter your username: ')
       rl.prompt()
     }
+  } else {
+    hospitalapp.getCommand(input, hospitalapp.loginPrivilege, rl, (data, message) => {
+      console.log(data)
+      rl.setPrompt(message)
+      rl.prompt()
+    })
   }
 })
