025a6681e58e0f6f7bd60ff6aacfda59f6425521
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-02-15 22:49:45 +0700

    revision

--- hospital.js
+++ hospital.js
@@ -1,5 +1,5 @@
 let fs       = require('fs')
-let readline = require('readline-sync')
+let readline = require('readline')
 
 class Hospital {
   constructor(name, location, employees, patients) {
@@ -20,17 +20,52 @@ class Patient {
 
 class Employee {
   constructor(name, position, username, password) {
-    this.name     = namename
+    this.name     = name
     this.position = position
     this.username = username
     this.password = password
   }
 }
 
+class Login {
+  constructor(username, password) {
+    this._username = username
+    this._password = password
+    this._name
+    this._privilege
+  }
+
+  isValid() {
+    return this._username != '' && this._password != ''
+  }  
+
+  checkAccount(accounts) {
+    console.log(accounts)
+    if (this.isValid()) {
+      for (let i = 0; i < accounts.length; i++) {
+        if (this._username === accounts[i].username && this._password === accounts[i].password) {
+          this.privilege = accounts[i].position
+          this.name      = accounts[i].name
+          return true 
+        }
+      }
+      return false
+    }
+    return false
+  } 
+
+  get privileged() {
+    return {name:this.name, privilege:this.privilege}
+  }
+
+}
+
 class HospitalApp {
-  constructor() {
-    this.hospital          = new Hospital('Lunatic', 'Subconsciousness', 200, 100)
-    this._loggedInEmployee = null
+  constructor(name, location, employees, patients) {
+    this.hospital          = new Hospital(name, location, employees, patients)
+    this._loginStatus
+    this._loggedInEmployee
+    this._loginPrivillege
   }
   
   fileHandler(option = 'read', fileName) {
@@ -43,13 +78,13 @@ class HospitalApp {
         break;
       case 'write':
         return (text) => {
-          fs.writeFileSync(filename, text, (err) => {
+          fs.writeFileSync(fileName, text, (err) => {
             if (err) {
               throw err
             }
           })
         }
-        
+        break;
     }
   }
   
@@ -58,11 +93,11 @@ class HospitalApp {
   }
   
   createEmployee(name,position,username,password) {
-    let newEmployee     = new Employee(name, position, username, password)
     let oldEmployee     = this.fileHandler('read', 'employees.json')
     let prepareData     = this.fileHandler('write', 'employees.json') 
-    oldEmployee.push(newEmployee)
-    prepareData(oldEmployee)
+    oldEmployee.push(new Employee(name,position,username,password))
+    prepareData(JSON.stringify(oldEmployee))
+    
   }
   
   getEmployees() {
@@ -73,26 +108,39 @@ class HospitalApp {
     return this.getEmployee()[index]
   }
   
-  login() {
-    let username = readline.question('Please enter your username: ')
-    let password = readline.question('Please enter your password: ')
-      
-    for (let i in this.getEmployees()) {
-      let uname = this.getEmployee(i).username
-      let pass  = this.getEmployee(i).password
-      if (username === uname && password === pass) {
-        if (this.getEmployee(i).position !== 'office-boy') {
-          this.loggedInEmployee = this.getEmployee(i)
-          this.appMenu()
-        }
-        console.log('We are sorry, but you have no access..')
+  login(username, password) {
+    let employees = this.getEmployees()
+    return password => {
+      let login = new Login(username, password)
+      if(login.checkAccount(employees)) {
+        this._loginStatus      = true
+        this._loginPrivillege  = login.privileged.privilege
+        this._loggedInEmployee = login.privileged.name
+        // console.log(this._loginStatus)
+        return this._loginStatus
       }
+      return false
     }
+    // let username = readline.question('Please enter your username: ')
+    // let password = readline.question('Please enter your password: ')
+      
+
+    // for (let i in this.getEmployees()) {
+    //   let uname = this.getEmployee(i).username
+    //   let pass  = this.getEmployee(i).password
+    //   if (username === uname && password === pass) {
+    //     if (this.getEmployee(i).position !== 'office-boy') {
+    //       this.loggedInEmployee = this.getEmployee(i)
+    //       this.appMenu()
+    //     }
+    //     console.log('We are sorry, but you have no access..')
+    //   }
+    // }
   }
   
   appMenu() {
     console.log('_____________________________________________')
-    console.log(`Welcome ${this.loggedInEmployee.name}, your access level is: ${this.loggedInEmployee.position}`)
+    console.log(`Welcome ${this._loggedInEmployee}, your access level is: ${this._loginPrivillege}`)
     console.log('What would you like to do?')
     console.log('Options:')
     console.log('list_patients')
@@ -102,7 +150,38 @@ class HospitalApp {
   }
 }
 
-let hospitalapp = new HospitalApp()
+let hospitalapp = new HospitalApp('Lunatic', 'Subconsciousness', 200, 150)
 
 // hospitalapp.login()
-hospitalapp.createEmployee('Danang', 'doctor', 'danang', 'admindanang')
+// hospitalapp.createEmployee('Danang', 'doctor', 'danang', 'admindanang')
+
+let rl = readline.createInterface({
+  input : process.stdin,
+  output: process.stdout
+})
+
+let login
+let loginData = new Object()
+console.log('Welcome')
+rl.setPrompt('Please enter your username: ')
+rl.prompt()
+
+rl.on('line', (input) => {
+  if (!loginData.hasOwnProperty('username')) {
+    loginData['username'] = input
+    login = hospitalapp.login(loginData.username)
+    rl.setPrompt('Please enter your password: ')
+    rl.prompt()
+  } else if (!loginData.hasOwnProperty('password')) {
+    loginData['password'] = input
+    if (login(loginData.password)) {
+      rl.setPrompt(hospitalapp.appMenu())
+      rl.prompt()
+    } else {
+      loginData = new Object()
+      console.log('Lu salah account cuk')
+      rl.setPrompt('Please enter your username: ')
+      rl.prompt()
+    }
+  }
+})
