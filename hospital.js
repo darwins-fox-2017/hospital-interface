0b6648c6223f046844e1d2da37383f85f1f0ab55
Eri Irawan <eryirawan91@gmail.com>
2017-02-16 13:50:33 +0700

    revisi login

--- hospital.js
+++ hospital.js
@@ -8,23 +8,114 @@ const rl       = readline.createInterface({
 })
 
 class Hospital {
-  constructor(name, location, employees, patients) {
+  constructor(name, location) {
     this.name = name
-    this.employees = employees
-    this.patients = patients
-    this.location = location
+    this.employeesPosition = ["Doctor","Admin","Receptionist","OB"]
+    this.employeesName = ["Eri","Budi","Raisa","Sayuti"]
+    this.username = ["doctor","admin","recep","ob"]
+    this.password = ["doctor","admin","recep","ob"]
+    this.patients = ["dani","dono","danu","dini"]
+    this.diagnose = ["demam","flu","sakit perut","migran"]
+    this.id
+  }
+
+  welcome(){
+    console.log(`=====================================`);
+    console.log(`Welcome too ${this.name} Hospital ${this.location}`);
+    console.log(`======================================`);
+    this.login()
   }
 
   home(){
     console.log(`====================================================`);
-    // console.log(`Welcome ${this.emName}. Your access level is: ${this.employees}`);
+    console.log(`Welcome ${this.employeesName}. Your access level is: ${this.employeesPosition}`);
     console.log(`====================================================`);
     console.log(`Option:`);
     console.log(`1. List Patients`);
     console.log(`2. View Record`);
     console.log(`3. Add Record`);
     console.log(`4. Remove Recod`)
+    this.getProcess()
   }
+
+  login(){
+    rl.question(`Please enter your username\n`, (answer) => {
+      if (this.username.indexOf(answer) !== -1) {
+        this.id = this.username.indexOf(answer)
+        this.getPassword()
+      }else {
+        this.login()
+      }
+    })
+  }
+
+  getPassword(){
+    rl.question(`Please enter your password\n`, (answer)=>{
+      if (answer === this.password[this.id]) {
+        this.employeesName = this.employeesName[this.id]
+        this.employeesPosition = this.employeesPosition[this.id]
+        this.home()
+      }else {
+        this.getPassword()
+      }
+    })
+  }
+
+  list(){
+    console.log(`Id | Name   | Diagnose`);
+    console.log(`======================`);
+    for (let i = 0; i < this.patients.length; i++) {
+      console.log(`${i + 1}  | ${this.patients[i]}    | ${this.diagnose[i]}`);
+    }
+    this.home()
+  }
+
+  view(){
+    rl.question(`Enter id patient : `, (answer) => {
+      console.log(`Name : ${this.patients[answer - 1]}\nDiagnose : ${this.diagnose[answer - 1]}`);
+      this.home()
+    })
+  }
+
+  add(){
+    rl.question(`Enter name & diagnose <ex: name, diagnose> : `, (answer)=>{
+      let splitAnswer = answer.split(',')
+      this.patients.push(splitAnswer[0])
+      this.diagnose.push(splitAnswer[1])
+      this.home()
+    })
+  }
+
+  remove(){
+    rl.question(`Enter id patients : `, (answer) => {
+      this.patients.splice(answer-1, 1)
+      this.diagnose.splice(answer-1, 1)
+      this.home()
+    })
+  }
+
+  getProcess(){
+    rl.question(`What would you like to do ?`, (answer) => {
+      switch (answer) {
+        case "1":
+          this.list()
+          break;
+        case "2":
+          this.view()
+          break;
+        case "3":
+          this.add()
+          break;
+        case "4":
+          this.remove()
+          break;
+        default:
+          console.log(`Please choose 1-4`);
+          this.getProcess()
+      }
+    })
+  }
+
 }
 
 class Patient {
@@ -42,30 +133,7 @@ class Employee {
     this.username = username
     this.password = password
   }
-
-  writeToFile(){
-    fs.writeFileSync(file, JSON.stringify(this.listData, null, "\t"), "utf-8")
-  }
-
-  inputToFile(){
-    let idLast = 1
-    if (typeof this.listData[this.listData -1] !== 'undefined') {
-      idLast  = this.listData[this.listData.length -1].id + 1
-    }
-
-    this.listData.push({"id":idLast, "name":this.name, "position":this.position, "username":this.username, "password":this.password})
-  }
 }
 
-const file     = 'employee.json'
-const readFile = fs.readFileSync(file, 'utf-8')
-
-let parseData  = []
-if (readFile) {
-  parseData = JSON.parse(readFile)
-}
-console.log(readFile);
-let employees = new Employee(parseData)
-
-let hospital = new Hospital()
-    hospital.home()
+let hospital = new Hospital('Hacktiv8','Jakarta')
+    hospital.login()
